9d11db8ff73353d6ca6bd27a883e11553a454ad3
micolin <mico_lin@foxmail.com>
2014-11-29 18:49:59 +0800

    Complete song crawler song_downloader.py

--- wangyiMusic/song_downloader.py
+++ wangyiMusic/song_downloader.py
@@ -27,19 +27,58 @@ def get_page(url):
 class Song:
 	def __init__(self,song_id,html=''):
 		self.song_id = song_id
+		dom = etree.HTML(html)
 		self.html = html
-	
-	def __get_basic_info(self):
-		pass
+		self.song_name,self.singer_id,self.singer_name,self.album_id,self.album_name = self.__get_basic_info(dom)
+		self.song_lrc = self.__get_lrc(dom)
+		self.sim_songs = self.__get_sim_song(dom)
 
-	def __get_action_info(self):
-		pass
+	def __get_basic_info(self,dom_tree):
+		name_dom = dom_tree.xpath(u"//em[@class='f-ff2']")[0]
+		song_name = name_dom.text.encode('utf8')
+		
+		singer_dom = dom_tree.xpath(u"//a[@class='s-fc7']|//span[@class='s-fc7']")
+		singer_id = 'none'
+		try:
+			singer_href = singer_dom[0].attrib['href']
+			singer_id = singer_href[singer_href.index('=')+1:]
+		except:
+			logging.info("There's no id for singer")
 
-	def __get_lrc(self):
-		pass
-	
-	def __get_sim_song(self):
-		pass
+		singer_name = singer_dom[0].text.encode('utf8')
+		
+		album_id = 'none'
+		try:
+			album_href = singer_dom[1].attrib['href']
+			album_id = album_href[album_href.index('=')+1:]
+		except:
+			logging.info("There's no id for album")
+		album_name = singer_dom[1].text.encode('utf8')
+		return song_name,singer_id,singer_name,album_id,album_name
+
+	def __get_lrc(self,dom_tree):
+		song_lrc = ''
+		try:
+			lrc_dom = dom_tree.xpath(u"//div[@class='bd bd-open f-brk f-ib']")[0]
+			for lrc in lrc_dom.itertext():
+				song_lrc += lrc.strip().encode('utf8')
+		except:
+			logging.info("There's no lrc for song:%s"%(self.song_id))
+		
+		if "暂时没有歌词" in song_lrc:
+			song_lrc = 'none'
+
+		return song_lrc
+
+	def __get_sim_song(self,dom_tree):
+		song_list = []
+		sim_song_dom = dom_tree.xpath(u"//a[@class='s-fc1']")
+		for sim_song in sim_song_dom:
+			song_list.append(sim_song.attrib['data-res-id'])
+		return song_list
+
+	def data_in_string(self):
+		return "%s\t"*8%(self.song_id,self.song_name,self.singer_id,self.singer_name,self.album_id,self.album_name,self.song_lrc,','.join(self.sim_songs))
 
 def get_playlists(filepath):
 	playlist_songs = {}
@@ -53,16 +92,29 @@ def get_playlists(filepath):
 
 song_url_template = "http://music.163.com/song?id=%s"
 
-def get_all_song():
-	playlist_songs = get_playlists('./data/playlist.basic_info')
+def get_all_song(playlist_info_file):
+	logging.info("Song info crawling process >> begin")
+	playlist_songs = get_playlists(playlist_info_file)
 	for (playlist_id,song_list) in playlist_songs.items():
 		for song_id in song_list:
 			song_url = song_url_template%(song_id)
-			song_page = get_page(song_url)
-			song = Song(song_id,song_page)
-			print song
-			break
+			logging.info('Crawl song page of song:%s url:%s'%(song_id,song_url))
+			try:
+				song_page = get_page(song_url)
+				if song_page :
+					try:
+						song = Song(song_id,song_page)
+						print song.data_in_string()
+					except Exception,e:
+						logging.error('Parsing song page:%s failed...'%(song_url))
+				else:
+					logging.error("Get song page failed...")
+			except Exception, e:
+				logging.error("Crawl song page %s Failed"%(song_id))
 		break
+	logging.info("Song info crawling process >> complete")
 
 if __name__=="__main__":
-	get_all_song()
+	args = sys.argv
+	playlist_info_file = args[1]
+	get_all_song(playlist_info_file)
