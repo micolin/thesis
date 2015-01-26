257d3e817eb771d05ce2c3ad02fca5d2ed7a339f
micolin <mico_lin@foxmail.com>
2015-01-26 23:54:34 +0800

    Modify run_all_model.sh

--- experiment/run_all_model.sh
+++ experiment/run_all_model.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
-SET_LEVEL=500_6
 TRAIN_PROB=0.7
 E_TYPE='song'
-TOP_N=50
-RESULT_ROOT_DIR='./song_dataset/result_top'${TOP_N}
+TOPIC_NUM=1500
 
-#Baseline
-#python random_select.py $SET_LEVEL $TRAIN_PROB $E_TYPE > ./song_dataset/result_more/rs_${SET_LEVEL}_${TRAIN_PROB} &
-#python popularity.py $SET_LEVEL $TRAIN_PROB $E_TYPE > ./song_dataset/result_more/pop_${SET_LEVEL}_${TRAIN_PROB} &
-#python item_CF.py $SET_LEVEL $TRAIN_PROB $TOP_N $E_TYPE > ${RESULT_ROOT_DIR}/${SET_LEVEL}/itemCF_${SET_LEVEL}_${TRAIN_PROB} &
-#python user_CF.py $SET_LEVEL $TRAIN_PROB $TOP_N $E_TYPE > ${RESULT_ROOT_DIR}/${SET_LEVEL}/userCF_${SET_LEVEL}_${TRAIN_PROB} &
+for set_level in "500_1"
+do 
+	'''
+	>>>>---Baseline--<<<<
+	'''
+	python random_select.py $set_level $TRAIN_PROB $E_TYPE > ./song_dataset/result_more/rs_${set_level}_${TRAIN_PROB} 
+	python popularity.py $set_level $TRAIN_PROB $E_TYPE > ./song_dataset/result_more/rs_${set_level}_${TRAIN_PROB}
 
-#MyMethod
-#python item_tag_CF.py $SET_LEVEL $TRAIN_PROB $TOP_N > ${RESULT_ROOT_DIR}/${SET_LEVEL}/itemTagCF_${SET_LEVEL}_${TRAIN_PROB} &
-python user_tag_CF.py $SET_LEVEL $TRAIN_PROB $TOP_N > ${RESULT_ROOT_DIR}/${SET_LEVEL}/userTagCF_${SET_LEVEL}_${TRAIN_PROB} &
-#python userLDA.py $SET_LEVEL $TRAIN_PROB 200 $TOP_N $E_TYPE > ${RESULT_ROOT_DIR}/${SET_LEVEL}/userLDA_${SET_LEVEL}_${TRAIN_PROB}_200topic &
-#python userLDA.py $SET_LEVEL $TRAIN_PROB 1500 $TOP_N $E_TYPE > ${RESULT_ROOT_DIR}/${SET_LEVEL}/userLDA_${SET_LEVEL}_${TRAIN_PROB}_1500topic &
-#python userLDA.py $SET_LEVEL $TRAIN_PROB 2000 $TOP_N $E_TYPE > ${RESULT_ROOT_DIR}/${SET_LEVEL}/userLDA_${SET_LEVEL}_${TRAIN_PROB}_2000topic &
-#python userLDA.py $SET_LEVEL $TRAIN_PROB 2500 $TOP_N $E_TYPE > ${RESULT_ROOT_DIR}/userLDA_${SET_LEVEL}_${TRAIN_PROB}_2500topic &
+	for top_n in "50"
+	do
+		RESULT_ROOT_DIR='./song_dataset/result_top'${top_n}
+		'''
+		>>>>---Baseline--<<<<
+		'''
+		#python item_CF.py $set_level $TRAIN_PROB $top_n $E_TYPE > ${RESULT_ROOT_DIR}/${set_level}/itemCF_${set_level}_${TRAIN_PROB} 
+		python user_CF.py $set_level $TRAIN_PROB $top_n $E_TYPE > ${RESULT_ROOT_DIR}/${set_level}/userCF_${set_level}_${TRAIN_PROB} 
+
+		'''
+		>>>>---SingleModel--<<<<
+		'''
+		#python item_tag_CF.py $set_level $TRAIN_PROB $top_n > ${RESULT_ROOT_DIR}/${set_level}/itemTagCF_${set_level}_${TRAIN_PROB}
+		python user_tag_CF.py $set_level $TRAIN_PROB $top_n > ${RESULT_ROOT_DIR}/${set_level}/userTagCF_${set_level}_${TRAIN_PROB}
+		python userLDA.py $set_level $TRAIN_PROB $TOPIC_NUM $top_n $E_TYPE > ${RESULT_ROOT_DIR}/${set_level}/userLDA_${set_level}_${TRAIN_PROB}_${TOPIC_NUM}topic
+
+		'''
+		>>>>---HybirdModel--<<<<
+		'''
+		for recommend_job in "mix_sim" "mix_result" "mix_sim_reorder" "mix_result_reorder"
+		do
+			python hybirdModel.py $set_level $TRAIN_PROB $TOPIC_NUM $top_n $recommend_job > ${RESULT_ROOT_DIR}/${set_level}/hybirdModel_${set_level}_${TRAIN_PROB}_${recommend_job}
+			
+			python ubase_hybirdModel.py $set_level $TRAIN_PROB 'tag' $recommend_job $top_n > ${RESULT_ROOT_DIR}/${set_level}/ub_hybird_tag_${set_level}_${TRAIN_PROB}_${recommend_job}
+
+			python ubase_hybirdModel.py $set_level $TRAIN_PROB 'lda' $recommend_job $top_n $TOPIC_NUM > ${RESULT_ROOT_DIR}/${set_level}/ub_hybird_lda_${set_level}_${TRAIN_PROB}_${recommend_job}
+		done
+	done
+done
